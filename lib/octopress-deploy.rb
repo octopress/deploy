0be88d018ae32c2c2477bb0a1ff205c8e101a930
Parker Moore <parkrmoore@gmail.com>
2014-02-11 23:42:16 -0500

    Only accept input if it's TTY.
    /cc @imathis

--- lib/octopress-deploy.rb
+++ lib/octopress-deploy.rb
@@ -100,27 +100,39 @@ FILE
     end
 
     def self.ask_bool(message)
-      ask(message, ['y','n']) == 'y'
+      if_tty_otherwise(true, message) do
+        ask(message, ['y','n']) == 'y'
+      end
     end
 
     def self.ask(message, valid_options)
-      if valid_options
-        options = valid_options.join '/'
-        answer = get_stdin("#{message} [#{options}]: ").downcase.strip
-        if valid_options.map{|o| o.downcase}.include?(answer)
-          return answer
+      if_tty_otherwise(false, message) do
+        if valid_options
+          options = valid_options.join '/'
+          answer = get_stdin("#{message} [#{options}]: ").downcase.strip
+          if valid_options.map{|o| o.downcase}.include?(answer)
+            return answer
+          else
+            return false
+          end
         else
-          return false
+          answer = get_stdin("#{message}: ")
         end
-      else
-        answer = get_stdin("#{message}: ")
+        answer
       end
-      answer
     end
           
     def self.get_stdin(message)
       print message
       STDIN.gets.chomp
+    end
+
+    def self.if_tty_otherwise(default, message)
+      if $stdout.tty?
+        yield
+      else
+        puts "Assuming '#{default}' for '#{message}'."
+      end
     end
   end
 end
