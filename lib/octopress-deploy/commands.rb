7abdacf2915f4c791707a424b5a040ce3db7bdc7
Brandon Mathis <brandon@imathis.com>
2014-02-20 09:43:31 -0600

    reordered command options

--- lib/octopress-deploy/commands.rb
+++ lib/octopress-deploy/commands.rb
@@ -5,9 +5,9 @@ module Octopress
         p.command(:deploy) do |c|
           c.syntax "octopress deploy [options]"
           c.description "Deploy your Octopress site."
-          c.option "using", "--using METHOD", "Define the push method to use, overriding your configuration file's setting"
           c.option "config_file", "--config FILE", "The path to your config file (default: _deploy.yml)"
           c.option "init", "--init METHOD", "Initialize a config file with the options for the given method."
+          c.option "using", "--using METHOD", "Define the push method to use, overriding your configuration file's setting"
           c.option "pull", "--pull DIRECTORY", "Pull down the published copy of your site into a directory (default: ./site-pull)"
 
           c.action do |_, options|
